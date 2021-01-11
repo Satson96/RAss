15c6424a8ecf93795f8d929726c54cca969024a7
Dmytro Satskyi <satskyi96@gmail.com>
2021-01-11 20:43:07 +0200

    Initial Commit

---
+++ Reddit-TopItems-Client_RTestAss/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reddit-TopItems-Client_RTestAss
+//
+//  Created by Dmytro Satskyi on 11.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
